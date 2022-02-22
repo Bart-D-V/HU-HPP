2f517ad8efe82aa19ea83c66d1778419e1d25806
Bartbabart <Bart.de.vries@outlook.com>
2022-02-22 17:56:06 +0100

    inleveropgave 2: functie voor aantal digits in int en distribution_pass functie

---
+++ opdracht1.2/src/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using std::cout, std::endl;
+
+int main()
+{
+    int a = 367;
+    cout << a / 10 << endl;
+}
---
+++ test/src/include/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using std::cout, std::endl;
+
+int main()
+{
+    int a = 367;
+    cout << a / 100 << endl;
+    return 0;
+}
